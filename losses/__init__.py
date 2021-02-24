33ae6b561adfac1e2127753896bd29bd54e765d8
StevRamos <stevhr@gmail.com>
2021-02-24 11:27:04 -0500

    fixed 1

--- losses/__init__.py
+++ losses/__init__.py
@@ -4,7 +4,7 @@ def L2_norm(image_rec, image_orig):
 
   assert image_rec.shape == image_orig.shape, "Assertion error: shape of input should be as same as target"
   
-  return torch.lignalg.norm(image_rec.reshape(image_rec.shape[0], -1) - image_orig.reshape(image_orig.shape[0], -1), ord=2, dim=1).mean()
+  return torch.linalg.norm(image_rec.reshape(image_rec.shape[0], -1) - image_orig.reshape(image_orig.shape[0], -1), ord=2, dim=1).mean()
 
 #  image_rec_r = image_rec.view(-1,64*64*3)
 #  image_orig_r = image_orig.view(-1,64*64*3)
@@ -20,7 +20,7 @@ def L1_norm(encoder,encoder_rec):
 
   assert encoder.shape == encoder_rec.shape, "Assertion error: shape of input should be as same as target"
   
-  return torch.lignalg.norm(encoder.reshape(encoder.shape[0], -1) - encoder_rec.reshape(encoder_rec.shape[0], -1), ord=1, dim=1).mean()
+  return torch.linalg.norm(encoder.reshape(encoder.shape[0], -1) - encoder_rec.reshape(encoder_rec.shape[0], -1), ord=1, dim=1).mean()
 
 
 
