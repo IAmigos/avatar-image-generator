56c502b8baf009527fbbabef0d9edf4d839a5b48
StevRamos <stevhr@gmail.com>
2022-02-22 20:25:30 -0500

    train.py ready to use sweep

--- train.py
+++ train.py
@@ -9,6 +9,14 @@ CONFIG_FILENAME = "config.json"
 PROJECT_WANDB = "avatar_image_generator"
 ENTITY = "iamigos"
 
+
+def is_there_arg(args, master_arg):
+    if(master_arg in args):
+        return True
+    else:
+        return False
+
+
 def train(config_file, use_wandb, run_name, run_notes):
     set_seed(32)
     config = configure_model(config_file, use_wandb)
@@ -24,9 +32,16 @@ def train(config_file, use_wandb, run_name, run_notes):
 
 
 if __name__ == '__main__':
-    args = parse_arguments()
-    use_wandb = args.wandb
-    run_name = args.run_name
-    run_notes = args.run_notes
+    use_sweep = is_there_arg(sys.argv, '--use_sweep')
+
+    if not use_sweep:
+        args = parse_arguments()
+        use_wandb = args.wandb
+        run_name = args.run_name
+        run_notes = args.run_notes
+    else:
+        use_wandb = True
+        run_name = None
+        run_notes = None
 
     train(CONFIG_FILENAME, use_wandb=use_wandb, run_name=run_name, run_notes=run_notes)
